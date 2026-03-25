1a3578e90f7879bed0602ee12688290289f7a76b
amaterasusodd-cmyk <amaterasusodd@gmail.com>
2026-03-25 10:38:14 -0400

    created an initializer to make the cards

---
+++ cards.py
@@ -0,0 +1,13 @@
+# Quick function to create the cards for the game
+cardslist=[]
+# adds card values (ace,jack,queen,king=1,11,12,13)
+for i in range(1,14):
+    cardvalue=[[i],[i],[i],[i]]
+    cardslist.append(cardvalue)
+# appends each card with a suit
+for i in cardslist:
+    i[0].append('s') # Spade
+    i[1].append('c') # Club
+    i[2].append('h') # Heart
+    i[3].append('d') # Diamond
+print(cardslist)
