93398cf1342ba710115b65e0fd601b9f684c69f7
amaterasusodd-cmyk <amaterasusodd@gmail.com>
2026-03-30 11:09:05 -0400

    updated main with game select (basic)

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import pygame
-
-pygame.init()
-
-pygame.quit()
+gamechoice=int(input("Please select a game: 1: Blackjack, 2: Poker, 3: Solitaire, 0: Quit"))
+if gamechoice==1:
+    pygame.init()
+    # run blackjack (in development)
+elif gamechoice==2:
+    pygame.init()
+    # need to implement poker
+elif gamechoice==3:
+    pygame.init()
+    # need to implement solitaire
+else:
+    print("Goodbye.")
+    pygame.quit()
 
