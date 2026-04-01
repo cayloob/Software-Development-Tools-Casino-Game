74193a0d3aa0e725a9e7b44ac326a4891effde34
amaterasusodd-cmyk <amaterasusodd@gmail.com>
2026-04-01 10:58:15 -0400

    updating deck shuffle

--- cards.py
+++ cards.py
@@ -6,14 +6,22 @@ def makecards():
     for i in range(1,14):
         cardvalue=[[i,'s'],[i,'c'],[i,'h'],[i,'d']]
         cardslist.append(cardvalue)
+    # TODO: make cardslist one dimensional for convenience
     return cardslist
 class Deck():
     def __init__(self,deckamnt=1):
-        pass
+        self._cardslist=makecards()
     def draw():
         #random.randint()
-        pass
     def shuffle():
-        pass
+        # randomizes the order of cards in the deck
+        # METHOD: make second list of cards
+        # move cards into 1 dimensional list in random order
+        newcardset=[]
+        for card in self._cardslist:
+            position=random.randint(1,52)
+            newcard=[position,card[0],card[1]]
+        newcardset.sort()
+            
 
 
