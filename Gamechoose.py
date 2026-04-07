5b45a08606b5c577cb84e14a5ce7784390c8e6ae
amaterasusodd-cmyk <amaterasusodd@gmail.com>
2026-04-07 10:43:01 -0400

    linter fix

--- Gamechoose.py
+++ Gamechoose.py
@@ -1,12 +1,15 @@
 import pygame
-gamechoice=int(input("Please select a game: 1: Blackjack, 2: Poker, 3: Solitaire, 0: Quit"))
-if gamechoice==1:
+
+
+name = "Please select a game: 1: Blackjack, 2: Poker, 3: Solitaire, 0: Quit"
+gamechoice = int(input(name))
+if gamechoice == 1:
     pygame.init()
     # run blackjack (in development)
-elif gamechoice==2:
+elif gamechoice == 2:
     pygame.init()
     # need to implement poker
-elif gamechoice==3:
+elif gamechoice == 3:
     pygame.init()
     # need to implement solitaire
 else:
