23de4e87f5cd8133ce84f2112a215edf7914d76b
megankimball-creator <meganraykimball@gmail.com>
2026-04-07 10:36:11 -0400

    updated main.py (not important yet just messing with pygame)

--- main.py
+++ main.py
@@ -2,23 +2,34 @@ import pygame
 
 pygame.init()
 
-screen = pygame.display.set_mode((1280,720))
+'''AU_logo = pygame.image.load("AU_logo.png")
+logo_rect = AU_logo.get_rect()'''
 
-clock= pygame.time.Clock()
 
-while True:
-    # reads user input
+screen_width, screen_height = pygame.display.get_desktop_sizes()[0]
+screen = pygame.display.set_mode((screen_width, screen_height - 60))
+
+screen.fill("antiquewhite3")
+
+font = pygame.font.SysFont('roboto', 60)
+
+pygame.display.set_caption('The Alfred Casino')
+
+running = True
+clock = pygame.time.Clock()
+
+
+while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
-            raise SystemExit
-
-    #logic updates (to do later)
+            running = False
     
+    text = font.render("Welcome to the Alfred Casino", True, "white")
+    textrect = text.get_rect()
+    textrect.center = (screen_width/2, screen_height/6)
 
-    screen.fill("green")
+    screen.blit(text, textrect)
+    pygame.display.flip()  # refresh screen display
+    #clock.tick(60) # wait until next frame (runs 60FPS)
 
-    #Graphics render (to do)
-
-    pygame.display.flip() # refresh screen display
-    clock.tick(60) # wait until next frame (runs 60FPS)
+pygame.quit()
