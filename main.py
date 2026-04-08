9aba718ca50c99bef10ae2f94308a2e3889bfc12
Caleb Raughley <calebraughley@gmail.com>
2026-04-08 15:26:45 -0400

    Added comments to main

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ screen.fill("antiquewhite3")
 font = pygame.font.SysFont('roboto', 60)
 
 pygame.display.set_caption('The Alfred Casino')
-
+#Initializing all of the objects to be used in the program.
 clock= pygame.time.Clock()
 hand = BlackJackHand()
 deck = Deck()
@@ -30,30 +30,37 @@ while True:
         if event.type == pygame.QUIT:
             running = False
 
-        
+        #Check if the event type is a button press
         if event.type == pygame.KEYDOWN:
+            #If its an 's' Key then player wants to stand, run the stand code
             if event.key == pygame.K_s:
                 win = dealer.dealer_play(dealer_hand,hand,deck)
+            #If its an 'h' Key then player wants to hit, run the hit code
             if event.key == pygame.K_h:
                 hand.add_card(deck)
                 total = hand.check_total()
 
-
+    
     if win == -1:
+        #If the player lost run this code
         screen.fill("red")
     elif win == 1:
+        #If the player won run this code
         screen.fill("green")
     else:
+        #If it is in a normal state run this code
         screen.fill("black")
-
+    #Create the score text and blit to screen
     score_surface = font.render(f"Total: {total}", True, (255, 255, 255))
     screen.blit(score_surface, (10, 10))
 
     for i,card in enumerate(hand.cards_list):
+        #Making the player's cards appear on the screen
         card.create()
         screen.blit(card.image,((card.rect.center[0]+(10*i)),(card.rect.center[1]+(10*i))))
 
     for i,card in enumerate(dealer_hand.cards_list):
+        #Making the dealer's cards appear on the screen
         card.create()
         screen.blit(card.image,((card.rect.center[0]+(10*i)-100),(card.rect.center[1]+(10*i)-100)))
 
