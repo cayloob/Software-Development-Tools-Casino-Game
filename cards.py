8eaa36243a0e72c89a92f20e6248279e19b1e62d
Caleb Raughley <calebraughley@gmail.com>
2026-04-10 10:24:01 -0400

    Made cards draw based on value (all facecards or aces are drawn as jacks of hearts

--- cards.py
+++ cards.py
@@ -7,7 +7,7 @@ class Card(pygame.sprite.Sprite):
         super().__init__()
         self.value = value
         self.suit = suit
-        self.suit_num_to_str = {1:'Spades',2:'Hearts',3:'Diamonds',4:'Clubs'}
+        self.suit_num_to_str = {1:'spades',2:'hearts',3:'diamonds',4:'clubs'}
         
     def __str__(self):
         return str(self.value) + ' ' + str(self.suit_num_to_str[self.suit])
@@ -15,7 +15,10 @@ class Card(pygame.sprite.Sprite):
     def create(self):
         # Use this to make the sprite drawing images of the card this can be moved to the draw function
         # I moved this from __init__ because that broke the hands.py tests
-        name = "assets\AU_card_back.png.png"
+        if self.value >2 and self.value <11:
+            name = f'AU_card_deck\{self.suit_num_to_str[self.suit]}{self.value}.png'
+        else:
+            name = f'AU_card_deck\hearts_jack_unfinished.png'
         self.image = pygame.image.load(name).convert_alpha()
         self.rect = self.image.get_rect()
         self.rect.center = (640, 360) 
